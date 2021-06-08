333e70235b29bd6901e8f23e9f3aa59347ba9228
Aditi Kaushik <47269969+aditi-kaushik1@users.noreply.github.com>
2021-06-08 22:43:35 +0530

    Create Buy Please.java

---
+++ Buy Please.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		System.out.println(a*x + y*b);
+	}
+}
